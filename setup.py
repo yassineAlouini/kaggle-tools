b7d549aded92bff996bcb883fe2c0407768f8516
yassine <yassinealouini@outlook.com>
2016-08-21 18:38:28 +0200

    Add some metadata about the package in the setup.py file

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@ from setuptools import find_packages, setup
 NAME = 'kaggle_tools'
 VERSION = '0.0.1'
 AUTHOR = 'Yassine Alouini'
+DESCRIPTION = """This is a suite of tools to help you participate in various
+Kaggle competitions"""
+
 
 setup(
     name=NAME,
     version=VERSION,
-    author=AUTHOR,
     packages=find_packages(),
+    # Some metadata
+    author=AUTHOR,
+    description=DESCRIPTION,
+    license="MIT",
+    keywords="kaggle machine-learning",
 )
