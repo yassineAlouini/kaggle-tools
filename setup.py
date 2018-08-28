229e16049f0fc9a4f65fed754527879a8b38124f
Yassine <yassinealouini@outlook.com>
2018-08-28 20:42:58 +0200

    Single line for DESCRIPTION field

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import find_packages, setup
 NAME = 'kaggle_tools'
 VERSION = '0.0.3'
 AUTHOR = 'Yassine Alouini'
-DESCRIPTION = """This is a suite of tools to help you participate in various
-Kaggle competitions"""
+DESCRIPTION = """This is a suite of tools to help you participate in various Kaggle competitions"""
 EMAIL = "yassinealouini@outlook.com"
 URL = ""
 
